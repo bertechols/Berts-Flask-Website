64cc16ae66418df6c1bd02e7223c31483037425a
bertechols <bert.echols@icloud.com>
2025-06-05 12:14:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def contact():
     return render_template("contact.html")
 
 if __name__ == "__main__":
-import os
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
